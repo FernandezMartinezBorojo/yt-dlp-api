e0631a6e6e0e77bafda9a26c0971c0331c2fe1e6
Sadi <rirfen5@gmail.com>
2025-10-22 23:50:29 -0500

    server yt-dlp

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from yt_dlp import YoutubeDL
+
+app = Flask(__name__)
+
+@app.route("/download", methods=["POST"])
+def download():
+    data = request.get_json()
+    url = data.get("url")
+
+    ydl_opts = {"skip_download": True, "quiet": True}
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return jsonify(info)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
